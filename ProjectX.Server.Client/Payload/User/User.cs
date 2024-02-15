910ee6b37b702826c48a9ef2c733c7a76b90b3fe
Alexander <vsany@meta.ua>
2024-02-15 15:58:00 +0200

    [+] User payload

---
+++ ProjectX.Server.Client/Payload/User/User.cs
@@ -0,0 +1,16 @@
+namespace ProjectX.Server.Client.Payload.User;
+
+public sealed class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Phone { get; set; }
+
+    public string AvatarUrl { get; set; }
+    
+    public Resourse.Resourse Resourse { get; set; }
+}
