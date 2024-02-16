c2f667b92519e159f088352572f5acbad4117fbe
Alexander <vsany@meta.ua>
2024-02-16 22:50:33 +0200

    [-] removed resource type

--- ProjectX.Server.Client/Payload/Resourse/ResourseType.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectX.Server.Client.Payload.Resourse;
-
-public enum ResourseType
-{
-    BronzeMoney = 1,
-    SilverMoney = 2,
-    GoldMoney = 3,
-    PlatinumMoney = 4
-}
--- ProjectX.Server.Database/User/Resource/ResourceType.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectX.Server.Database.User.Resource;
-
-public enum ResourceType
-{
-    BronzeMoney = 1,
-    SilverMoney = 2,
-    GoldMoney = 3,
-    PlatinumMoney = 4
-}
